f572f6ab03d362065bf4895b6a708ca3ccc60040
nayemrabbi <nayemrabbi1@gmail.com>
2023-01-08 00:49:32 +0600

    :hamburger: Toggle burger menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -58,8 +58,9 @@ function animateSlides() {
       .addTo(controller);
   });
 }
-let mouse = document.querySelector(".cursor");
-let mouseText = mouse.querySelector("span");
+const mouse = document.querySelector(".cursor");
+const mouseText = mouse.querySelector("span");
+const burger = document.querySelector(".burger");
 
 function cursor(e) {
   let mouse = document.querySelector(".cursor");
@@ -84,6 +85,27 @@ function activeCursor(e) {
     gsap.to(".title-swipe", 1, { y: "100%" });
   }
 }
+
+function navToggle(e) {
+  if (!e.target.classList.contains("active")) {
+    e.target.classList.add("active");
+    gsap.to(".line1", 0.5, { rotate: "45", y: 5, background: "black" });
+    gsap.to(".line2", 0.5, { rotate: "-45", y: -5, background: "black" });
+    gsap.to("#logo", 1, { color: "black" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
+    document.body.classList.add("hide");
+  } else {
+    e.target.classList.remove("active");
+    gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to(".line2", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to("#logo", 1, { color: "white" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -10%)" });
+    document.body.classList.remove("hide");
+  }
+}
+
+burger.addEventListener("click", navToggle);
 window.addEventListener("mousemove", cursor);
 window.addEventListener("mouseover", activeCursor);
+
 animateSlides();
